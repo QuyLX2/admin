e881d08ed092bb8bd2d0ed21a06c653dd2e3c21e
duongdat98 <duongdat@coders.com>
2020-09-06 15:23:45 +0700

    merger views

--- Client/admin/src/App.js
+++ Client/admin/src/App.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
+import UserListManagement from './views/pages/UserListManagement';
+import MarkTable from './views/pages/MarkTable';
+import AddNewPost from './views/pages/AddNewPost';
 
 class App extends Component {
   render() {
     return (
       <div>
-         My Addmin App
+        <MarkTable/>
+        <UserListManagement/>
+        <AddNewPost/>
       </div>
     );
   }
 }
 
 export default App;
+
