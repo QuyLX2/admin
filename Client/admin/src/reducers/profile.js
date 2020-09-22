35d81e71a411c39cbbb2e8ece62a020ef742ea27
quylx2 <lexuanquy4198@gmail.com>
2020-09-22 19:16:12 +0700

    jdshdgas

--- Client/admin/src/reducers/profile.js
+++ Client/admin/src/reducers/profile.js
@@ -33,7 +33,7 @@ export default function (state = initialState, action) {
         case CLEAR_PROFILE:
             return {
                 ...state,
-                profile: null,
+                // profile: null,
                 error: {}
             };
         case GET_PROFILE:
